82312c0e70f0f61b306c57cad06be2ef3d064b28
Guillermo Valverde <guillermovalverde42@gmail.com>
2024-10-17 15:02:11 -0600

    api de accesos/creacion registro

--- accesos/api/acceso_vehicular/controllers/create_access.php
+++ accesos/api/acceso_vehicular/controllers/create_access.php
@@ -5,13 +5,10 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
 
 require_once '../../venv.php';
-require_once BASE_PATH . '/postgresql/conexion.php';
+require_once BASE_PATH . '/mysql/conexion.php';
+require_once '../models/AccessModel.php';
 
-require_once '../middlewares/auth_middleware.php';
-require_once '../services/record_fields_validator.php';
-require_once '../models/ReportModel.php';
-
-$cn = conectarPostgresql();
+$cn = conectar();
 if (!$cn) {
   http_response_code(500);
   echo json_encode(["success" => false, "message" => "No se pudo conectar a la base de datos"]);
@@ -20,35 +17,46 @@ if (!$cn) {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $data = json_decode(file_get_contents("php://input"), true);
-  $required_fields = [
-    'date', 
-    'simple_load', 
-    'full_load', 
-    'meta', 
-    'difference', 
-    'accumulated_difference', 
-    'available_units', 
-    'unloading_units', 
-    'long_trip_units', 
-    'units_in_maintenance', 
-    'units_no_operator', 
-    'total_units', 
-    'observations'
-  ];
 
-  $validationResult = validateRequiredFields($data, $required_fields);
-  if (!$validationResult['success']) {
+  // Verifica si el JSON se ha decodificado correctamente
+  if (json_last_error() !== JSON_ERROR_NONE) {
     http_response_code(400);
-    echo json_encode($validationResult);
+    echo json_encode(["success" => false, "message" => "JSON inválido: " . json_last_error_msg()]);
     exit;
   }
 
-  $reportModel = new ReportModel($cn);
+  $required_fields = [
+    'nombre_operador',
+    'placas',
+    'tipo_transporte',
+    'contenedor1',
+    'contenedor2',
+    'tipo_identificacion',
+    'carga_descarga',
+    'id_empresa',
+    'sellos',
+    'areas',
+    'motivo',
+    'usuario_creacion',
+    'fecha_creacion',
+    'fecha_entrada',
+    'estado_acceso',
+    'tipo_mov'
+  ];
+
+  // $validationResult = validateRequiredFields($data, $required_fields);
+  // if (!$validationResult['success']) {
+  //   http_response_code(400);
+  //   echo json_encode($validationResult);
+  //   exit;
+  // }
+
+  $accessModel = new AccessModel($cn);
 
   try {
-    $new_record = $reportModel->insertRecord($data);
+    $new_record = $accessModel->insert($data);
     http_response_code(201);
-    echo json_encode($new_record);
+    echo json_encode(["success" => true, "message" => "Registro insertado correctamente", "data" => $new_record]);
   } catch (Exception $e) {
     http_response_code(500);
     echo json_encode(["success" => false, "message" => "Error al insertar el registro: " . $e->getMessage()]);
---
+++ accesos/api/acceso_vehicular/models/AccessModel.php
@@ -0,0 +1,67 @@
+<?php
+require_once '../../venv.php';
+
+class AccessModel
+{
+  private mysqli|null $cn;
+
+  public function __construct(mysqli $cn = null)
+  {
+    $this->cn = $cn;
+    if (!$this->cn) {
+      throw new Exception("No se pudo conectar a la base de datos");
+    }
+  }
+
+  public function insert(array $data)
+  {
+    // Prepara la consulta
+    $query = "INSERT INTO phicargo.acceso_vehicular(
+                  nombre_operador, placas, tipo_transporte, contenedor1, contenedor2, 
+                  tipo_identificacion, carga_descarga, id_empresa, sellos, areas, 
+                  motivo, usuario_creacion, fecha_creacion, fecha_entrada, 
+                  estado_acceso, tipo_mov)
+              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+    $stmt = $this->cn->prepare($query);
+
+    if (!$stmt) {
+      throw new Exception("Error al preparar la consulta: " . $this->cn->error);
+    }
+
+    $stmt->bind_param(
+      'sssssssisssissss',
+      $data['nombre_operador'],
+      $data['placas'],
+      $data['tipo_transporte'],
+      $data['contenedor1'],
+      $data['contenedor2'],
+      $data['tipo_identificacion'],
+      $data['carga_descarga'],
+      $data['id_empresa'],
+      $data['sellos'],
+      $data['areas'],
+      $data['motivo'],
+      $data['usuario_creacion'],
+      $data['fecha_creacion'],
+      $data['fecha_entrada'],
+      $data['estado_acceso'],
+      $data['tipo_mov']
+    );
+
+    if (!$stmt->execute()) {
+      throw new Exception("Error al ejecutar la consulta: " . $stmt->error);
+    }
+
+    // Cierra el statement
+    $stmt->close();
+
+    return true;
+  }
+
+
+  public function __destruct()
+  {
+    $this->cn->close();
+  }
+}
